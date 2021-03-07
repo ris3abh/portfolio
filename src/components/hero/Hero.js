b143efda4e2c31d74561d3d63c46118767422877
Rishabh sharma <rishabh.sharma1103@gmail.com>
2021-03-07 12:50:20 +0530

    hero componeent

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,8 +4,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { useSpring, animated } from 'react-spring';
-import Icon from '@material-ui/core/Icon';
-
+import Fade from '@material-ui/core/Fade';
 import { makeStyles } from '@material-ui/core/styles';
 
 import './style.css' ;
@@ -21,13 +20,14 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
     },
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+
   rightGrid:{ 
     display: 'flex', 
+    alignItems: 'center', 
+    justifyContent: 'center' ,
+    marginTop:130,
+  },
+  leftGrid:{  
     alignItems: 'center', 
     justifyContent: 'center' ,
     marginTop:100,
@@ -38,8 +38,8 @@ const useStyles = makeStyles((theme) => ({
     color: 'white',
     fontSize: 15,
     fontWeight: 500,
-    background: 'linear-gradient(45deg, #FFA500 30%, #FF8C00 90%)',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    background: '#FF7F50',
+    boxShadow: 'px 1px 1px 2px #131313',
     height: 48,
   }
 }));
@@ -49,13 +49,13 @@ const Hero = () => {
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
   return (
     <Container>
-      <div className={classes.root}>
+      <div>
         <Grid container spacing={10}>
-          <Grid item xs={12} sm={6}>
-            <h1 style={{color: '#fff'}}>Hii,</h1>
+          <Grid item xs={12} sm={6} className={classes.leftGrid}>
+                  <h1 style={{color: '#fff'}}>Hii,</h1>
             <h1 style={{color: '#fff'}}>My Name Is Rishabh!</h1>
             <div className={classes.root}>
-                <Button variant="outlined" className = {classes.buttons}>
+                <Button variant="outlined"  className = {classes.buttons}>
                     Download Resume
                 </Button>
                 <Button variant="outlined" className = {classes.buttons} >
